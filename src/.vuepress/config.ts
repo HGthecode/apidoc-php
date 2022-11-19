a4f4860ade88f97e7b029f4aca08d8a0fa14cf37
HG <36032426+HGthecode@users.noreply.github.com>
2022-11-19 12:57:43 +0800

    更新搜索

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import { searchPlugin } from "@vuepress/plugin-search";
 import { path } from "@vuepress/utils";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 
 export default defineUserConfig({
@@ -13,7 +13,23 @@ export default defineUserConfig({
 
   theme,
   plugins: [
-    searchPlugin({}),
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          name: "category",
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          name: "tag",
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
   ],
   alias: {
     "@DownloadFe":  path.resolve(__dirname, "components/DownloadFe.vue"),
