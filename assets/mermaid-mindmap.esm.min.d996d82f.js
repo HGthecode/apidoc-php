0b063478f384743daf2f3f2c5a6aa935654e3ffb
HG <36032426+HGthecode@users.noreply.github.com>
2023-03-07 17:33:56 +0800

    更新下载地址

--- assets/mermaid-mindmap.esm.min.d996d82f.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app.21b7d189.js";const t="mindmap",d=a=>a.match(/^\s*mindmap/)!==null,i=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2.63543355.js"),[]);return{id:t,diagram:a}},m={id:t,detector:d,loader:i};export{m as default};
