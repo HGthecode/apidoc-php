57dc574089e3eac2705b44a70353967c0612e031
zzx <zhuzixian520@126.com>
2024-05-12 01:12:20 +0800

    enh

--- src/providers/Yii2Service.php
+++ src/providers/Yii2Service.php
@@ -5,6 +5,7 @@ namespace hg\apidoc\providers;
 use Yii;
 use yii\base\Component;
 use yii\base\InvalidConfigException;
+use yii\db\Exception;
 use yii\helpers\Inflector;
 
 class Yii2Service extends Component
@@ -48,38 +49,40 @@ class Yii2Service extends Component
      */
     static function registerRoute($route)
     {
-        $k = mb_substr($route['uri'], 1);
-        $v = Inflector::camel2id($k);
+        $rule_key = mb_substr($route['uri'], 1);
+        $rule_val = Inflector::camel2id($rule_key);
         $url_manager = Yii::$app->urlManager;
-        $url_manager->rules[$k] = $v;
+        $url_manager->rules[$rule_key] = $rule_val;
         $url_manager->init();
     }
 
+    /**
+     * @throws Exception
+     */
     static function databaseQuery($sql)
     {
-        // TODO: Implement databaseQuery() method.
+        Yii::$app->db->createCommand($sql)->execute();
     }
 
     static function getRootPath()
     {
         //dd(Yii::getAlias('@app'));
-        return Yii::getAlias('@app');
+        return Yii::getAlias('@app') . DIRECTORY_SEPARATOR;
     }
 
     static function getRuntimePath()
     {
-        //dd(Yii::getAlias('@runtime'));
         return Yii::getAlias('@runtime');
     }
 
     static function setLang($locale)
     {
-        // TODO: Implement setLang() method.
+        Yii::$app->language = $locale;
     }
 
     static function getLang($lang)
     {
-        // TODO: Implement getLang() method.
+        return Yii::t('apidoc', mb_substr($lang, mb_strlen('apidoc.')));
     }
 
     static function handleResponseJson($res)
